a660c894d14c85998150c008fa168043c961a214
harshit1255 <108613826+harshit1255@users.noreply.github.com>
2023-12-23 09:23:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function mapLetters(text)
+{   let obj ={};
+	let arr = text.split('');
+	for(let i=0;i<arr.length;i++)
+		{
+			if(obj[arr[i]])
+				obj[arr[i]].push(i);
+			else
+				obj[arr[i]] =[i];
+		}
+ return obj;
+}
